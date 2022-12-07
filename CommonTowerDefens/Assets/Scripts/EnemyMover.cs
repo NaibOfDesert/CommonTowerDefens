d5108b429bb1e9eada92fb97fe67deae4314f359
NaibOfDesert <tryc.artur@gmail.com>
2022-12-08 00:29:13 +0100

    Tiles update, enemy movement.

--- CommonTowerDefens/Assets/Scripts/EnemyMover.cs
+++ CommonTowerDefens/Assets/Scripts/EnemyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] [Range(0f, 5f)]float speed = 1f;
     
     void Start()
     {
@@ -15,12 +15,20 @@ public class EnemyMover : MonoBehaviour
 
     IEnumerator FollowPath()
     {
-
         foreach(var p in path)
         {
-            //Debug.Log(p.name);
-            transform.position = p.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = p.transform.position;
+            float travelpercent = 0f;
+            transform.LookAt(endPosition);
+
+            while(travelpercent < 1f)
+            {
+                travelpercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelpercent);
+                yield return new WaitForEndOfFrame();
+            }
+            
         }
     }
 }
